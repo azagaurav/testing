e8bc6f56f7c65fa5224f20b8fe6c568ca78e8e34
gaurav28384 <gauravkumar.singh@ireslab.com>
2018-02-03 17:37:38 +0530

    String example with new and literal

---
+++ src/main/java/com/test/string/NewString.java
@@ -0,0 +1,18 @@
+package com.test.string;
+
+public class NewString {
+	public static void main(String[] args) {
+
+		String a = "Java ";
+		String b = a + "1.8";
+		String c = a.concat("1.8");
+		String d= new String("Java 1.8");
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(c.equals(b));
+		System.out.println((c == b));
+		System.out.println(c.equals(d));
+		System.out.println((c == d));
+		System.out.println(a);
+	}
+}
