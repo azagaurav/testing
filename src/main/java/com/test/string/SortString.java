db7b8a0478149251dec8a45218d54f4b62b543e5
gaurav28384 <gauravkumar.singh@ireslab.com>
2018-02-03 18:09:36 +0530

    Sorting String with two for loop

---
+++ src/main/java/com/test/string/SortString.java
@@ -0,0 +1,29 @@
+package com.test.string;
+
+public class SortString {
+	public static void main(String[] args) {
+		String original = "edczxrdgh";
+		int j = 0;
+		char temp = 0;
+		System.out.println(original);
+		char[] chars = original.toCharArray();
+		for (int i = 0; i < chars.length; i++) {
+			System.out.println("I : " + chars[i]);
+			for (j = 0; j < chars.length; j++) {
+				System.out.println("J : " + chars[j]);
+				if (chars[j] > chars[i]) {
+					temp = chars[i];
+					chars[i] = chars[j];
+					chars[j] = temp;
+				}
+				System.out.println(chars);
+			}
+		}
+		StringBuilder sb = new StringBuilder();
+		for (int k = 0; k < chars.length; k++) {
+			sb.append(chars[k]);
+			System.out.println(chars[k]);
+		}
+		System.out.println(sb.toString());
+	}
+}
