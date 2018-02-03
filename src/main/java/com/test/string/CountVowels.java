94729dbce6fa6a905de9cc407fabf7c27d9e5be7
gaurav28384 <gauravkumar.singh@ireslab.com>
2018-02-03 14:42:29 +0530

    First Commit

---
+++ src/main/java/com/test/string/CountVowels.java
@@ -0,0 +1,22 @@
+package com.test.string;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class CountVowels {
+
+	public static void main(String args[]) throws IOException {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));// "This is a good work for our org";
+		System.out.println("Enter the String : ");
+		String text = bf.readLine();
+		int count = 0;
+		for (int i = 0; i < text.length(); i++) {
+			char c = text.charAt(i);
+			if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
+				count++;
+			}
+		}
+		System.out.println("There are" + " " + count + " " + "vowels");
+	}
+}
