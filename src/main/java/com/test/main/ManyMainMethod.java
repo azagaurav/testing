20a138df8296576d69d8e96bdfdda5a61c931ecf
gaurav28384 <gauravkumar.singh@ireslab.com>
2018-02-03 20:12:32 +0530

    Many Main Method in single class file

---
+++ src/main/java/com/test/main/ManyMainMethod.java
@@ -0,0 +1,13 @@
+package com.test.main;
+
+public class ManyMainMethod {
+	public static void main(String[] arg) {
+		System.out.println("ManyMainMethod");
+	}
+}
+
+class ManyMainMethod1 {
+	public static void main(String[] arg) {
+		System.out.println("ManyMainMethod1");
+	}
+}
